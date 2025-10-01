06dd203456dfcb17b90ef3250dc4fbc08f90d3b5
[DarlinHolguin] <[darlinholguin123@gmail.com]>
2025-10-01 15:10:15 -0400

    Second Function

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 //Code your solutions in this file
-function fiveToOneHundred () {
+function fiveToOneHundred() {
     for (let i = 5; i <= 100; i++) {
         console.log(i);
     }
 }
+
+const multiplesOfThree = () => {
+    for (let i = 1; i <= 100; i++) {
+        if (i % 3 === 0) {
+            console.log(i)
+        }
+    }
+};
+multiplesOfThree()
