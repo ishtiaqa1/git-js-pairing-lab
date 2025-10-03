294b07787630e1b1da015e28182ef13c073d9110
[DarlinHolguin] <[darlinholguin123@gmail.com]>
2025-10-03 13:01:54 -0400

    Q12 Function

--- index.js
+++ index.js
@@ -74,3 +74,10 @@ const printTime = () => {
     const seconds = date.getSeconds();
     console.log(`${hours}:${minutes}:${seconds}`);
 }
+
+const isLeapYear = (year) => {
+    if (year % 4 === 0 && year % 100 !== 0 || year % 400 === 0) {
+        return true
+    }
+    return false
+}
